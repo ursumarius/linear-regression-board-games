895ff1d4bc2a1a21307ec2de1c2f65886c5caf59
ursumarius <marius-git@outlook.com>
2024-06-09 09:11:49 +0200

    cleaning

--- data-ingest.py
+++
@@ -1,6 +0,0 @@
-import csv
-import requests
-with requests.Session() as s:
-    s.post(url, data=payload)
-    download = s.get('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-25/details.csv')
-
